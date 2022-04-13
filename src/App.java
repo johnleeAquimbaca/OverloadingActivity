2d52d75e9f53c3d870aa411875b89e86da343d53
johnleeAquimbaca <jlaquimbaca@gmail.com>
2022-04-13 20:29:25 +0800

    Modified the class App

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 public class App {
 /**
      * This method takes in two int parameters to add.
-     * 
      */
     private static int sum2(int num1, int num2){
         return num1 + num2;
@@ -9,37 +8,49 @@ public class App {
 
     /**
      * This method takes in 3 int parameters to add.
-     * 
      */
     private static int sum3(int num1, int num2, int num3){
         return num1 + num2 + num3;
     }
 
     /**
-     * Modify the methods in such a way that the Class App
-     * can now have 2 overloaded computeSum() methods.
-     * One computing the sum of two numbers, and one computing the sum
-     * of three numbers. 
+     * This is an Overloading method that compute the sum of 2 numbers
      */
-
+    private static int computeSum(int num1, int num2){
+    return num1 + num2;
+    }
+    /**
+     * This is an Overloading method that compute the sum of 3 numbers
+     */
+    private static int computeSum(int num1, int num2, int num3){
+    return num1 + num2 + num3;
+    }
 
     /**
-     * 
-     * Create method that returns the greater integer value
+     * This method returns the greater integer value
      */
-
-
+    private static int greaterValue(int a, int b){
+      if(a > b){
+        return a;
+        }else return b;
+    }
     /**
-     * 
-     * Create method that returns the greater double value
+     * This method returns the greater double value
      */
+    private static double greaterValue(double x, double y){
+        if(x > y){
+            return x;
+        }else return y;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         /**
          * Create Method calls that takes in 2 parameters, and 3 parameters
          * respectively and computes their sum.
          */
-
+        System.out.println("Computing 2 numbers 100 and 50 = "+App.computeSum(100, 50));
+        System.out.println("Computing 3 numbers 100, 50, and 20 = "+App.computeSum(100, 50, 20));
         /**
          * Call the greaterValue method with integer parameters
          */
